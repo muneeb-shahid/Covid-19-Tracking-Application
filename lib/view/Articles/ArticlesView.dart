998326cdc02c57bbc9af0ffae16c56a913c868e4
Muneeb Shahid <muneebshahid0102@gmail.com>
2023-10-27 22:06:43 +0500

    Twenty seven commit

--- lib/view/Articles/ArticlesView.dart
+++ lib/view/Articles/ArticlesView.dart
@@ -19,7 +19,7 @@ class ArticlesView extends StatelessWidget {
     final ref = FirebaseDatabase.instance.ref("Article");
 
     return Scaffold(
-      backgroundColor: App_Constants_Colors.app_pink_color,
+      backgroundColor: App_Constants_Colors.app_background_color,
       appBar: AppBar(
         title: TextHeading("Articles", App_Constants_Colors.app_white_color),
         elevation: 0,
@@ -49,109 +49,113 @@ class ArticlesView extends StatelessWidget {
                 shrinkWrap: true,
                 itemCount: snapshot.data!.snapshot.children.length,
                 itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Get.to(BlogFullPost(
-                        blogData: list[index],
-                      ));
-                    },
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Card(
-                          color: App_Constants_Colors.app_grey_color,
-                          child: 
-                          
-                          
-                           Column(
-                             children: [
-                               Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    width: double.infinity,
-                                    height: 200,
-                                    child: Image.network(
-                                      list[index]["image"].toString(),
-                                    ),
+                  return Column(
+                    children: [
+                     
+                      GestureDetector(
+                        onTap: () {
+                          Get.to(BlogFullPost(
+                            blogData: list[index],
+                          ));
+                        },
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Card(
+                            color: App_Constants_Colors.app_white_color_70,
+                            child: Column(
+                              children: [
+                                Image.network(
+                                  width: double.infinity,
+                                  height: screenHeight * 0.2,
+                                  list[index]["image"].toString(),
+                                  fit: BoxFit.cover,
+                                ),
+                                SizedBox(
+                                  height: screenHeight * 0.03,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    left: screenWidth * 0.05,
+                                    right: screenWidth * 0.05,
                                   ),
-                                ],
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        list[index]["title"].toString(),
+                                        overflow: TextOverflow.ellipsis,
+                                        softWrap: true,
+                                        maxLines: 2,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            letterSpacing: 2,
+                                            wordSpacing: 2,
+                                            color: App_Constants_Colors
+                                                .app_black_color,
+                                            fontFamily:
+                                                App_Fonts_Constants.Philosopher,
+                                            fontSize: 16.sp),
+                                      ),
+                                      SizedBox(
+                                        height: screenHeight * 0.03,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            list[index]['date'].toString(),
+                                            style: TextStyle(
+                                                color: App_Constants_Colors
+                                                    .app_black_color,
+                                                fontFamily:
+                                                    App_Fonts_Constants.Inter,
+                                                fontSize: App_Fonts_Constants
+                                                    .text_font_size.sp),
+                                          ),
+                                        ],
+                                      ),
+                                      const Divider(
+                                        thickness: 2,
+                                        color: Colors.grey,
+                                      ),
+                                      Text(
+                                        list[index]["content"].toString(),
+                                        overflow: TextOverflow.ellipsis,
+                                        softWrap: true,
+                                        maxLines: 3,
+                                        style: TextStyle(
+                                            color: App_Constants_Colors
+                                                .app_black_color,
+                                            fontFamily: App_Fonts_Constants.Poppins,
+                                            fontSize: 13.sp),
+                                      ),
+                                      SizedBox(
+                                        height: screenHeight * 0.03,
+                                      ),
+                                      customLogin_SignupButton(
+                                          innerColor:
+                                              App_Constants_Colors.app_green_color,
+                                          textColor:
+                                              App_Constants_Colors.app_white_color,
+                                          text: "Read more",
+                                          func: () => Get.to(BlogFullPost(
+                                                blogData: list[index],
+                                              ))),
+                                      SizedBox(
+                                        height: screenHeight * 0.03,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                             ],
-                           )
-                          //  ListTile(
-                          //   title: Column(
-                          //     children: [
-                          //       SizedBox(
-                          //         height: screenHeight * 0.3,
-                          //         width: double.infinity,
-                          //         child: Image.network(
-                          //           list[index]["image"].toString(),
-                          //         ),
-                          //       ),
-                          //       Text(
-                          //         list[index]["title"].toString(),
-                          //         overflow: TextOverflow.ellipsis,
-                          //         softWrap: true,
-                          //         maxLines: 1,
-                          //         style: TextStyle(
-                          //             fontWeight: FontWeight.bold,
-                          //             letterSpacing: 2,
-                          //             wordSpacing: 2,
-                          //             color: App_Constants_Colors.app_black_color,
-                          //             fontFamily: App_Fonts_Constants.Philosopher,
-                          //             fontSize: 16.sp),
-                          //       ),
-                          //       Row(
-                          //         mainAxisAlignment:
-                          //             MainAxisAlignment.spaceBetween,
-                          //         children: [
-                          //           Text(
-                          //             list[index]['date'].toString(),
-                          //             style: TextStyle(
-                          //                 color: App_Constants_Colors
-                          //                     .app_black_color,
-                          //                 fontFamily: App_Fonts_Constants.Inter,
-                          //                 fontSize: App_Fonts_Constants
-                          //                     .text_font_size.sp),
-                          //           ),
-                          //         ],
-                          //       ),
-                          //       const Divider(
-                          //         thickness: 2,
-                          //         color: Colors.grey,
-                          //       ),
-                          //       Text(
-                          //         list[index]["content"].toString(),
-                          //         overflow: TextOverflow.ellipsis,
-                          //         softWrap: true,
-                          //         maxLines: 3,
-                          //         style: TextStyle(
-                          //             color: App_Constants_Colors.app_black_color,
-                          //             fontFamily: App_Fonts_Constants.Poppins,
-                          //             fontSize: 13.sp),
-                          //       ),
-                          //       SizedBox(
-                          //         height: screenHeight * 0.03,
-                          //       ),
-                          //       customLogin_SignupButton(
-                          //           innerColor:
-                          //               App_Constants_Colors.app_black_color,
-                          //           textColor:
-                          //               App_Constants_Colors.app_white_color,
-                          //           text: "Read more",
-                          //           func: () => Get.to(BlogFullPost(
-                          //                 blogData: list[index],
-                          //               ))),
-                          //       SizedBox(
-                          //         height: screenHeight * 0.01,
-                          //       ),
-                          //     ],
-                          //   ),
-                          // ),
-
-                          ),
-                    ),
+                        ),
+                      ),
+                   SizedBox(
+                                      height: screenHeight * 0.02,
+                                    ),
+                    ],
                   );
                 },
               );
--- lib/view/BlogFullPost/BlogFullPost.dart
+++ lib/view/BlogFullPost/BlogFullPost.dart
@@ -1,13 +1,16 @@
+import 'package:covid_19_trackingapp/Reuseable%20Components/CustomButton/Login&signupButton.dart';
+import 'package:covid_19_trackingapp/Reuseable%20Components/ReUseable%20Drawer/ReUseableDrawer.dart';
+import 'package:covid_19_trackingapp/constants/TextHeading/text_heading.dart';
 import 'package:covid_19_trackingapp/constants/colors_constants/colors_constants.dart';
 import 'package:covid_19_trackingapp/constants/fonts_size_constant/fonts_size_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-
 class BlogFullPost extends StatelessWidget {
   final dynamic blogData;
-  const BlogFullPost({super.key, 
+  const BlogFullPost({
+    super.key,
     required this.blogData,
   });
 
@@ -17,70 +20,87 @@ class BlogFullPost extends StatelessWidget {
     return Scaffold(
       backgroundColor: App_Constants_Colors.app_background_color,
       appBar: AppBar(
-        backgroundColor: App_Constants_Colors.app_background_color,
         elevation: 0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20)),
+        ),
+        backgroundColor: App_Constants_Colors.app_green_color,
         leading: InkWell(
-          child: const Icon(
+          child: Icon(
             Icons.arrow_back_ios_new,
-            color: Colors.black,
+            color: App_Constants_Colors.app_white_color,
           ),
           onTap: () {
             Get.back();
           },
         ),
       ),
-      body: SafeArea(
-        top: true,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Column(
-              children: [
-
-                Text(
-                  blogData["title"].toString(),
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 2,
-                      wordSpacing: 2,
-                      color: App_Constants_Colors.app_black_color,
-                      fontFamily: App_Fonts_Constants.Philosopher,
-                      fontSize: 16.sp),
-                ),
-                SizedBox(
-                  height: heightt * 0.02,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      blogData['date'].toString(),
-                      style: TextStyle(
-                          color: App_Constants_Colors.app_black_color,
-                          fontFamily: App_Fonts_Constants.Poppins,
-                          fontSize: App_Fonts_Constants.text_font_size.sp),
-                    ),
-                  ],
-                ),
-                const Divider(
-                  thickness: 2,
-                  color: Colors.grey,
-                ),
-                Text(
-                  blogData["content"].toString(),
-                  style: TextStyle(
-                      color: App_Constants_Colors.app_black_color,
-                      fontFamily: App_Fonts_Constants.Poppins,
-                      fontSize: 13.sp),
-                ),
-              ],
-            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: screenWidth * 0.03,
+            right: screenWidth * 0.03,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: screenHeight * 0.02,
+              ),
+              Image.network(
+                width: double.infinity,
+                height: screenHeight * 0.2,
+                blogData["image"].toString(),
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: screenHeight * 0.02,
+              ),
+              Text(
+                blogData["title"].toString(),
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                    wordSpacing: 2,
+                    color: App_Constants_Colors.app_black_color,
+                    fontFamily: App_Fonts_Constants.Philosopher,
+                    fontSize: 16.sp),
+              ),
+              SizedBox(
+                height: heightt * 0.02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    blogData['date'].toString(),
+                    style: TextStyle(
+                        color: App_Constants_Colors.app_black_color,
+                        fontFamily: App_Fonts_Constants.Poppins,
+                        fontSize: App_Fonts_Constants.text_font_size.sp),
+                  ),
+                ],
+              ),
+              const Divider(
+                thickness: 2,
+                color: Colors.grey,
+              ),
+              Text(
+                blogData["content"].toString(),
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    letterSpacing: 2,
+                    wordSpacing: 2,
+                    color: App_Constants_Colors.app_black_color,
+                    fontFamily: App_Fonts_Constants.Poppins,
+                    fontSize: 13.sp),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
-
-
